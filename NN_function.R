aa6e9a1ccf9e4f9dfaef47fb19a0f40ca53a549d
diletta-ferri <126989831+diletta-ferri@users.noreply.github.com>
2023-08-25 19:50:24 +0200

    Update NN_function.R

--- NN_function.R
+++ NN_function.R
@@ -3,10 +3,26 @@ library(caret)
 library(pROC)
 
 
-NEURALNETWORK <- function(type,target,train_data,test_data,results) {
+NEURALNETWORK <- function(type,target,train_data,test_data,results,name_encoding) {
+  
+  results <- cbind(results, name_encoding = c(0,0,0,0,0)) #nel dataframe results aggiungo una colonna che si chiama come il nome dell'encoding e inizializzo i valori a 0
   
   all_columns <- names(train_data)
   features <- setdiff(all_columns, target)
+  
+  #mi assicuro che tutte le colonne siano numeriche (faccio i controlli su tutte tranne la target, sto assumendo che nei casi di regressione sia giusta e numerica)
+  non_numeric_columns <- sapply(train_data[,features], function(x) !is.numeric(x))
+  non_numeric_columns <- names(non_numeric_columns)[non_numeric_columns]
+  for (col in non_numeric_columns) {
+    train_data[[col]] <- as.numeric(train_data[[col]])
+  }
+  
+  non_numeric_columns_test <- sapply(test_data[,features], function(x) !is.numeric(x))
+  non_numeric_columns_test <- names(non_numeric_columns_test)[non_numeric_columns_test]
+  for (col in non_numeric_columns_test) {
+    test_data[[col]] <- as.numeric(test_data[[col]])
+  }
+
   
   #per la normalizzazione (salvo in columns_with_only_0_1 i nomi delle colonne one-hot-encoded)
   columns_with_only_0_1 <- character(0)
@@ -36,13 +52,15 @@ NEURALNETWORK <- function(type,target,train_data,test_data,results) {
       
       #NN:
       nn_model <- neuralnet(formula, data = scaled_train_data, hidden = 10, threshold=0.1, rep=3, linear.output = TRUE)
-      pr_nn <- predict(nn_model, rep="best", newdata = scaled_test_data)
-      #Tolgo la normalizzazione (questa era la formula per annullare la normalizzazione come l'avevo fatta io, ovviamente in caso va cambiata):
-      unscaled_predicted <- pr.nn * scaling_factors[2,target] + scaling_factors[1,target] 
-      #Calcolo RMSE:
-      rmse <- sqrt(mean((unscaled_predicted- test_data[[target]])^2))
-      #aggiungo il risultato al dataframe o quello che sarà in results
-      #da decidere come fare
+      for (i in 1:5) {
+        pr_nn <- predict(nn_model, rep=i, newdata = scaled_test_data) 
+        #Tolgo la normalizzazione (questa era la formula per annullare la normalizzazione come l'avevo fatta io, ovviamente in caso va cambiata):
+        unscaled_predicted <- pr.nn * scaling_factors[2,target] + scaling_factors[1,target] 
+        #Calcolo RMSE:
+        rmse <- sqrt(mean((unscaled_predicted- test_data[[target]])^2))
+        results[i,name_encoding] <- rmse
+      }
+      
     },
     
     "bin_classification" = {
@@ -61,18 +79,20 @@ NEURALNETWORK <- function(type,target,train_data,test_data,results) {
       }
       
       #NN:
-      nn_model <- neuralnet(formula, data = scaled_train_data, hidden = 10, threshold=0.1, rep=3, linear.output = FALSE)
-      pr_nn_probs <- predict(nn_model, rep="best", newdata = scaled_test_data) #da le probabilità di ognuni classe
-      #rendo uniformi le classi predette e quella del test, per poter calcolare l'AUC
-      column_names <- unique(test_data[[target]])
-      max_column_indices <- max.col(pr_nn_probs)
-      pred_class_names <- column_names[max_column_indices]
-      pred_class_names <- as.numeric(pred_class_names)
-      #Calcolo AUC:
-      roc_obj <- roc(test_data[[target]], pred_class_names)
-      auc_score <- auc(roc_obj)
-      #aggiungo il risultato al dataframe o quello che sarà in results
-      #da decidere come fare
+      nn_model <- neuralnet(formula, data = scaled_train_data, hidden = 10, threshold=0.1, rep=5, linear.output = FALSE)
+      for (i in 1:5) {
+        pr_nn_probs <- predict(nn_model, rep=i, newdata = scaled_test_data) #da le probabilità di ognuni classe
+        #rendo uniformi le classi predette e quella del test, per poter calcolare l'AUC
+        column_names <- unique(test_data[[target]])
+        max_column_indices <- max.col(pr_nn_probs)
+        pred_class_names <- column_names[max_column_indices]
+        pred_class_names <- as.numeric(pred_class_names)
+        #Calcolo AUC:
+        roc_obj <- roc(test_data[[target]], pred_class_names)
+        auc_score <- auc(roc_obj)
+        results[i,name_encoding] <- auc_score #salva nella colonna corrispondente i risultati della rete
+      }
+
     },
     
     "multiclass" = {
@@ -92,14 +112,17 @@ NEURALNETWORK <- function(type,target,train_data,test_data,results) {
       
       #NN:
       train_data[[target]] <- as.factor(train_data[[target]]) #mi assicuro che sia in livelli, se lo sono già tutte non è necessario
-      nn_model <- neuralnet(formula, data = scaled_train_data, hidden = 10, threshold=0.1, rep=3, linear.output = FALSE)
-      pr_nn_probs <- predict(nn_model, rep="best", newdata = scaled_test_data)
-      colnames(pr_nn_probs) <- unique(test_data[[target]])
-      #Calcolo AUNU:
-      library(mlr3measures)
-      aunu_score <- mauc_aunu(test_data[[target]], pr_nn_probs)
-      #aggiungo il risultato al dataframe o quello che sarà in results
-      #da decidere come fare
+      nn_model <- neuralnet(formula, data = scaled_train_data, hidden = 10, threshold=0.1, rep=5, linear.output = FALSE)
+      
+      for (i in 1:5) {
+        pr_nn_probs <- predict(nn_model, rep=i, newdata = scaled_test_data) #da le probabilità di ognuni classe
+        colnames(pr_nn_probs) <- unique(test_data[[target]])
+        #Calcolo AUNU:
+        library(mlr3measures)
+        aunu_score <- mauc_aunu(test_data[[target]], pr_nn_probs)
+        results[i,name_encoding] <- auc_score
+      }
+
     }
   )
   
